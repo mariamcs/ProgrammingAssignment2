32624e64e397b4be749f000f925a232a022dd118
mariamcs <m.heidari.b@gmail.com>
2016-08-28 20:50:54 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,46 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+#The first function, makeCacheMatrix is exactly like amkeVector
+# it creates an inverse "matrix" by using following functions:
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inveresed matrix
+#get the value of the inveresed matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv_mat <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_mat <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv_mat <<- inverse
+  getInverse <- function() inv_mat
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+#The following function is exactly the function provided in the example
+# it produces the inverese of the matrix created with the above function. 
+#However, it first checks to see  if the inverese matrix has already been produced 
+#If so, it gets the inveresed matrix from the cache 
+#Otherwise, it produces the inverese of the matrix 
+#then it changes the value of the inveresed matrix in the cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv_mat <- x$getInverse()
+  if (!is.null(inv_mat)) {
+    message("Getting the Cached Inveresed Matrix")
+    return(inv_mat)
+  }
+  mat <- x$get()
+  inv_mat <- solve(mat, ...)
+  x$setInverse(inv_mat)
+  inv_mat
 }
